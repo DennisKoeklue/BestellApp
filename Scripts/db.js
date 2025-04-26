a89bb0336dcbc193019ae44ede1752d53048a634
Dennis Koeklue <dennis.koeklue@gmail.com>
2025-04-26 14:15:33 +0200

    TestCommit

---
+++ Scripts/db.js
@@ -0,0 +1,145 @@
+
+
+
+let myDishes = [
+    {
+        "name": "Spaghetti Bolognese",
+        "price": 12.99,
+        "description": "Nach Tradition von der nonna"
+    },
+    {
+        "name": "Spaghetti mix fungi",
+        "price": 12.99,
+        "description": "Frisch gepflugte Stein Pilzen, Pfefferlinge mit einer Cremigen Sahnesoße "
+    },
+    {
+        "name": "Lasagne",
+        "price": 13.99,
+        "description": "Nach der Tradition von der Nonna"
+    },
+    {
+        "name": "Gnocchi-Auflauf",
+        "price": 11.99,
+        "description": "Frische Tomaten soße (Tomaten aus sizilien) mit handgemachten Gnocchis dazu oben drauf mit Buffel-Mozzerela getopt."
+    },
+    {
+        "name": "Spaghetti Carbonara",
+        "price": 10.99,
+        "description": "Guanciale-Speck aus italien mit gut Gereiften Pecorino Romano und Bio-eiern für das Cremige Etwas. "
+    },
+    {
+        "name": "Pasta Alfredo",
+        "price": 8.99,
+        "description": "eine Cremige soße aus Bio-Butter, Gutgereiften Pecorino Romano und Hartweizen-Spaghetti aus italien"
+    },
+    {
+        "name": "Pasta alla Norma",
+        "price": 12.99,
+        "description": "Pasta alla Norma ist ein echter Klassiker der sizilianischen Küche, fruchtigen Tomatensoße, frittierten Auberginen und gesalzenem Ricotta"
+    },
+    {
+        "name": "Rigatoni al forno",
+        "price": 12.99,
+        "description": "Meine Rigatoni al forno schmecken wie beim Italiener "
+    },
+    {
+        "name": "Burrata-Sandwich",
+        "price": 12.99,
+        "description": "Als Belag Tomaten, Rucola, gekochten Schinken und – na klar – Burrata und anschließend mit Pesto bestreichen."
+    },
+    {
+        "name": "Tortellini al Forno",
+        "price": 10.99,
+        "description": "Tortellini al Forno wie beim Italiener"
+    },
+
+]
+
+
+let myDessert = [
+    {
+        "name": "Tiramisu",
+        "price": 5.99,
+        "description": "Die köstliche Mischung aus Löffelbiskuit mit starkem Espresso, frischer Mascarponecreme und einem Hauch aromatischem Kakao ist einfach zu gut. Ein Dolce, das auch als kleiner Kuchen durchgehen könnte. Das Geheimnis liegt in der Kombination der Zutaten, lässt sich aber ganz einfach zubereiten."
+    },
+    {
+        "name": "Panna Cotta",
+        "price": 5.99,
+        "description": "Traditionell wird die Sahne mit Zucker und Gewürzen so lange erhitzt, bis sie stark reduziert ist. Weil diese Zubereitung Zeit und ein bisschen Übung braucht, wird das traditionelle Rezept heute oft mit Gelatine zubereitet."
+    },
+    {
+        "name": "Amarenakirschen aus dem Glas",
+        "price": 6.99,
+        "description": "Mit Amarenakirschen bezeichnen wir in Sirup eingelegte italienische Sauerkirschen – weil die Kirschsorte amarena heißt"
+    },
+    {
+        "name": "Zabaione",
+        "price": 5.99,
+        "description": "Zabaione, auch Zabaglione genannt, ist eine italienische Weinschaumcreme."
+    }
+    
+]
+
+
+
+
+
+
+
+
+
+let myDrinksNonAlk = [
+    {
+        "name":"Alkoholfreier Limoncello Spritz",
+        "price": 4.99
+    },
+    {
+        "name":"Coca Cola 0.5l",
+        "price": 3.99
+    },
+    {
+        "name":"Fanta 0.5l",
+        "price": 3.99
+    },
+    {
+        "name":"Sprite 0.5l",
+        "price": 3.99
+    },
+    {
+        "name":"Italienischer Kaffee 0.3l",
+        "price": 3.99
+    },
+    {
+        "name":"Lipton Ice tea Peach 0.2l",
+        "price": 2.99
+    },
+    {
+        "name":"Coke Zero",
+        "price": 3.99
+    },
+    {
+        "name":"Mountin Dew",
+        "price": 3.99
+    },
+]
+
+let myDrinksAlk = [
+    {
+    "name": "Limoncello Spritz",
+    "price": 4.59
+},
+{
+    "name": "Manhattan Little Italy",
+    "price": 7.99
+},
+{
+    "name": "Hot Limoncello",
+    "price": 7.99
+},
+{
+    "name": "Negroni Sbagliato",
+    "price": 8.99
+}
+
+
+]
