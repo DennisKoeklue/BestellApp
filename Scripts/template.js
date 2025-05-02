c025af637bd1aa981bc9e64371abf698a8f7c4d6
Dennis Koeklue(linux) <dennis.koeklue@gmail.com>
2025-05-02 15:34:45 +0200

    added code in template data

---
+++ Scripts/template.js
@@ -0,0 +1,68 @@
+function templateMainDishes() {
+    return`
+    <div class="maindish_divs">
+                        <div class="maindish_info">
+                        <h3>Spaghetti Bolognese</h3>
+                        <span>Nach Tradition von der nonna</span>
+                        <span class="price_span">19.99</span>
+                        </div>
+                        <div class="maindish_add_div">
+                            <div class="maindish_add_img">
+                                <span class="add_symbole">+</span>
+                            </div>
+                        </div>
+                    </div>
+    `
+}
+
+
+function templateDessert() {
+    return`
+    <div class="maindish_divs">
+                        <div class="maindish_info">
+                        <h3>Spaghetti Bolognese</h3>
+                        <span>Nach Tradition von der nonna</span>
+                        <span class="price_span">19.99</span>
+                        </div>
+                        <div class="maindish_add_div">
+                            <div class="maindish_add_img">
+                                <span class="add_symbole">+</span>
+                            </div>
+                        </div>
+                    </div>
+    `
+}
+
+function templateNonAlk() {
+    return`
+    <div class="maindish_divs">
+                        <div class="maindish_info">
+                        <h3>Spaghetti Bolognese</h3>
+                        <span>Nach Tradition von der nonna</span>
+                        <span class="price_span">19.99</span>
+                        </div>
+                        <div class="maindish_add_div">
+                            <div class="maindish_add_img">
+                                <span class="add_symbole">+</span>
+                            </div>
+                        </div>
+                    </div>
+    `
+}
+
+function templateAlkohl() {
+    return`
+    <div class="maindish_divs">
+                        <div class="maindish_info">
+                        <h3>Spaghetti Bolognese</h3>
+                        <span>Nach Tradition von der nonna</span>
+                        <span class="price_span">19.99</span>
+                        </div>
+                        <div class="maindish_add_div">
+                            <div class="maindish_add_img">
+                                <span class="add_symbole">+</span>
+                            </div>
+                        </div>
+                    </div>
+    `
+}
